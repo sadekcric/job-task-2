c261e79556d35b333320ad5056cc2633b0fc28ca
sadekcric4 <sadekcric4@gmail.com>
2024-08-14 21:01:57 +0600

    mongodb setup done

--- index.js
+++ index.js
@@ -13,6 +13,29 @@ app.get("/", () => {
   console.log("server is Running");
 });
 
+// Mongodb database started
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.1ekltq6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+// Mongodb database ended
+
 app.listen(port, () => {
   console.log(`Port is running at: ${port}`);
 });
